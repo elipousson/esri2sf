24912fa5ca80a0f9bed55dd7310e14e81e611918
Eli Pousson <eli.pousson@gmail.com>
2023-03-23 13:47:41 -0400

    refactor: remove unused .rlang_purrr_map_mold helper

--- R/standalone-purrr.R
+++ R/standalone-purrr.R
@@ -13,15 +13,6 @@ map <- function(.x, .f, ...) {
 }
 
 #' @keywords internal
-#' @importFrom rlang as_function global_env
-.rlang_purrr_map_mold <- function(.x, .f, .mold, ...) {
-  .f <- rlang::as_function(.f, env = rlang::global_env())
-  out <- vapply(.x, .f, .mold, ..., USE.NAMES = FALSE)
-  names(out) <- names(.x)
-  out
-}
-
-#' @keywords internal
 #' @importFrom rlang as_function global_env set_names
 map2 <- function(.x, .y, .f, ...) {
   .f <- rlang::as_function(.f, env = rlang::global_env())
